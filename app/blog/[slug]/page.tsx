0b35f0d0e6564c668ec9f494ca3b2e62e06bc37d
inpendently <hongmy172@gmail.com>
2026-02-03 09:47:04 +0700

    update v1

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -12,7 +12,7 @@ export default function Page({ params }: Params) {
   return (
     <>
       <h1>Slug: {params.slug}</h1>
-      <p>Hole Tex</p>
+      <p>HoleTex</p>
       <p>Welcome to Ha Noi</p>
     </>
   );

f946c13b9335b611cd83421ddedc63f97b9eac80
inpendently <hongmy172@gmail.com>
2026-02-05 17:16:48 +0700

    gg

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -12,7 +12,7 @@ export default function Page({ params }: Params) {
   return (
     <>
       <h1>Slug: {params.slug}</h1>
-      <p>Xinchao</p>
+      <p>Xin chao</p>
       <p>Welcome to Ha Noi</p>
     </>
   );
